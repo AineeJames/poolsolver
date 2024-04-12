887436fe93d7648eae1d74db9c7c73812480ed86
James Wilcock <jameswilcock8@gmail.com>
2024-04-11 20:21:59 -0700

    show mouse pos coords

--- src/main.c
+++ src/main.c
@@ -30,15 +30,32 @@ void init_balls(Ball *balls) {
 }
 
 int main(int argc, char *argv[]) {
-  InitWindow(600, 600, "Pool Sim");
+  InitWindow(1700, 900, "Pool Sim");
 
   Ball balls[16] = {0};
   init_balls(&balls[0]);
+
+  Texture2D table_texture = LoadTexture("assets/pool_table.png");
 
   while (!WindowShouldClose()) {
     BeginDrawing();
     // DrawPoolTable
     ClearBackground(RED);
+    DrawTexturePro(table_texture,
+                   (Rectangle){.x = 0,
+                               .y = 0,
+                               .width = table_texture.width,
+                               .height = table_texture.height},
+                   (Rectangle){.x = 0,
+                               .y = 0,
+                               .width = GetScreenWidth(),
+                               .height = GetScreenHeight()},
+                   (Vector2){0, 0}, 0, WHITE);
+    Vector2 mouse_pos = GetMousePosition();
+    const char *mouse_pos_str =
+        TextFormat("Pos = %d,%d\n", (int)mouse_pos.x, (int)mouse_pos.y);
+    DrawText(mouse_pos_str, 0, 0, 30, BLUE);
+
     EndDrawing();
   }
   return EXIT_SUCCESS;
