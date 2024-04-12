2c2657d82eda8248f88df47490b7d4410a0f64aa
James Wilcock <jameswilcock8@gmail.com>
2024-04-11 19:54:58 -0700

    ball enum

--- src/main.c
+++ src/main.c
@@ -1,10 +1,24 @@
 #include <raylib.h>
 #include <stdlib.h>
 
+typedef enum {
+  REG_BALL,
+  CUE_BALL,
+} BallType;
+
+typedef struct {
+  Vector2 position;
+  BallType type;
+} Ball;
+
 int main(int argc, char *argv[]) {
   InitWindow(600, 600, "Pool Sim");
+  Ball balls[16] = {0};
+  balls[0].type = CUE_BALL;
+
   while (!WindowShouldClose()) {
     BeginDrawing();
+    // DrawPoolTable
     ClearBackground(RED);
     EndDrawing();
   }
