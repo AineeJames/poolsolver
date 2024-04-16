b7396a28d6e0aa82fa12761914b5eb5bffbf5cb4
James Wilcock <jameswilcock8@gmail.com>
2024-04-15 20:50:14 -0700

    added wasm building command

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 BINARY_NAME="pool_sim"
-CFLAGS="-Werror -Wall -fsanitize=address -fsanitize=undefined `pkg-config --cflags raylib`"
-LIBS="-lm `pkg-config --libs raylib`"
+CFLAGS="-Werror -Wall -fsanitize=address -fsanitize=undefined $(pkg-config --cflags raylib)"
+LIBS="-lm $(pkg-config --libs raylib)"
 
 clang $CFLAGS src/*c -o $BINARY_NAME $LIBS
 
@@ -8,6 +8,9 @@ if [[ $? == 0 ]]; then
 	echo "Compiled $BINARY_NAME successfully!"
 	if [[ "$1" = "run" ]]; then
 		./$BINARY_NAME
+	elif [[ "$1" = "wasm" ]]; then
+		echo "Compiling wasm build"
+		emcc -o html_build/pool.html src/*.c -Os -Wall html_build/libraylib.a -Ihtmlbuild/ -I/usr/include/ -Lhtmlbuild/ -s ASYNCIFY -s USE_GLFW=3 -DPLATFORM_WEB --preload-file assets
 	fi
 else
 	echo "Failed to compile $BINARY_NAME!"
