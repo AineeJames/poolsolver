3694a4ce9295056091c88e17e59898215a880bcf
AineeJames <olsenaiden33@gmail.com>
2024-04-15 22:00:27 -0600

    fixed wasm build used pck-config

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ if [[ $? == 0 ]]; then
 		./$BINARY_NAME
 	elif [[ "$1" = "wasm" ]]; then
 		echo "Compiling wasm build"
-		emcc -o html_build/pool.html src/*.c -Os -Wall html_build/libraylib.a -Ihtmlbuild/ -I/usr/include/ -Lhtmlbuild/ -s ASYNCIFY -s USE_GLFW=3 -DPLATFORM_WEB --preload-file assets
+		emcc -o html_build/pool.html src/*.c -Os -Wall html_build/libraylib.a -Ihtmlbuild/ -I/usr/include/ -Lhtmlbuild/ -s ASYNCIFY -s USE_GLFW=3 -DPLATFORM_WEB --preload-file assets $(pkg-config --cflags raylib)
 	fi
 else
 	echo "Failed to compile $BINARY_NAME!"
